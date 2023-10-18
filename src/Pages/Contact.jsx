cf995c14f1ff5ed962edb452c8dc0b51ee6781fb
prasish07 <prasishshrestha0099@gmail.com>
2023-10-18 10:28:06 +0545

    Change the name of contact

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -5,98 +5,98 @@ import "./contact.css";
 import Button from "../components/Button";
 
 const Contact = () => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
-  const [message, setMessage] = useState("");
+	const [firstName, setFirstName] = useState("");
+	const [lastName, setLastName] = useState("");
+	const [email, setEmail] = useState("");
+	const [phone, setPhone] = useState("");
+	const [message, setMessage] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+	const handleSubmit = (e) => {
+		e.preventDefault();
 
-    alert("Thank you for contacting me!!!");
-  };
-  return (
-    <div className={`${styles.flexStart} ${styles.paddingX}`}>
-      <div
-        className={`${styles.boxWidth} flex justify-center items-center h-[100vh]`}
-      >
-        <div className="container">
-          <div className="text">Contact us</div>
-          <form onSubmit={handleSubmit}>
-            <div className="form-row">
-              <div className="input-data">
-                <input
-                  type="text"
-                  required
-                  value={firstName}
-                  onChange={(e) => setFirstName(e.target.value)}
-                />
-                <div className="underline"></div>
-                <label>First Name</label>
-              </div>
-              <div className="input-data">
-                <input
-                  type="text"
-                  required
-                  value={lastName}
-                  onChange={(e) => setLastName(e.target.value)}
-                />
-                <div className="underline"></div>
-                <label>Last Name</label>
-              </div>
-            </div>
-            <div className="form-row">
-              <div className="input-data">
-                <input
-                  type="text"
-                  required
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-                <div className="underline"></div>
-                <label>Email Address</label>
-              </div>
-            </div>
-            <div className="form-row">
-              <div className="input-data">
-                <input
-                  type="phone"
-                  required
-                  value={phone}
-                  onChange={(e) => setPhone(e.target.value)}
-                />
-                <div className="underline"></div>
-                <label>Phone number</label>
-              </div>
-            </div>
-            <div className="form-row">
-              <div className="input-data textarea">
-                <textarea
-                  rows="8"
-                  cols="80"
-                  required
-                  value={message}
-                  onChange={(e) => setMessage(e.target.value)}
-                ></textarea>
-                <br />
-                <div className="underline"></div>
-                <label>Write your message</label>
-              </div>
-            </div>
-            <div className="mt-4 flex justify-center">
-              <button
-                type="submit"
-                className={`py-4 px-6 font-poppins font-medium text-[18px] outline-none bg-black rounded-xl text-white`}
-              >
-                Submit
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
-  );
+		alert("Thank you for contacting me!!!");
+	};
+	return (
+		<div className={`${styles.flexStart} ${styles.paddingX}`}>
+			<div
+				className={`${styles.boxWidth} flex justify-center items-center h-[100vh]`}
+			>
+				<div className="container">
+					<div className="text">Contact me</div>
+					<form onSubmit={handleSubmit}>
+						<div className="form-row">
+							<div className="input-data">
+								<input
+									type="text"
+									required
+									value={firstName}
+									onChange={(e) => setFirstName(e.target.value)}
+								/>
+								<div className="underline"></div>
+								<label>First Name</label>
+							</div>
+							<div className="input-data">
+								<input
+									type="text"
+									required
+									value={lastName}
+									onChange={(e) => setLastName(e.target.value)}
+								/>
+								<div className="underline"></div>
+								<label>Last Name</label>
+							</div>
+						</div>
+						<div className="form-row">
+							<div className="input-data">
+								<input
+									type="text"
+									required
+									value={email}
+									onChange={(e) => setEmail(e.target.value)}
+								/>
+								<div className="underline"></div>
+								<label>Email Address</label>
+							</div>
+						</div>
+						<div className="form-row">
+							<div className="input-data">
+								<input
+									type="phone"
+									required
+									value={phone}
+									onChange={(e) => setPhone(e.target.value)}
+								/>
+								<div className="underline"></div>
+								<label>Phone number</label>
+							</div>
+						</div>
+						<div className="form-row">
+							<div className="input-data textarea">
+								<textarea
+									rows="8"
+									cols="80"
+									required
+									value={message}
+									onChange={(e) => setMessage(e.target.value)}
+								></textarea>
+								<br />
+								<div className="underline"></div>
+								<label>Write your message</label>
+							</div>
+						</div>
+						<div className="mt-4 flex justify-center">
+							<button
+								type="submit"
+								className={`py-4 px-6 font-poppins font-medium text-[18px] outline-none bg-black rounded-xl text-white`}
+							>
+								Submit
+							</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Contact;
